2731d209733774c3ba2327770abc14b065bd887d
Ilona Bekoeva <beklona30@gmail.com>
2022-12-13 17:45:54 +0300

    визуальный интерфейс

--- IronPainter/FormMenu.Designer.cs
+++ IronPainter/FormMenu.Designer.cs
@@ -28,20 +28,275 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBoxRussian = new System.Windows.Forms.PictureBox();
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.pictureBoxOssetian = new System.Windows.Forms.PictureBox();
+            this.pictureBoxArmenian = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGeorgian = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxRussianWord = new System.Windows.Forms.TextBox();
+            this.textBoxOssetianWord = new System.Windows.Forms.TextBox();
+            this.textBoxArmenianWord = new System.Windows.Forms.TextBox();
+            this.textBoxGeorgianWord = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonSelection = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonSaveResult = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRussian)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOssetian)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxArmenian)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGeorgian)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBoxRussian
+            // 
+            this.pictureBoxRussian.Location = new System.Drawing.Point(611, 27);
+            this.pictureBoxRussian.Name = "pictureBoxRussian";
+            this.pictureBoxRussian.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxRussian.TabIndex = 0;
+            this.pictureBoxRussian.TabStop = false;
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.Font = new System.Drawing.Font("Bebas Neue Bold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonResult.Location = new System.Drawing.Point(611, 649);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(250, 66);
+            this.buttonResult.TabIndex = 1;
+            this.buttonResult.Text = "ПОСМОТРЕТЬ РЕЗУЛЬТАТ";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxOssetian
+            // 
+            this.pictureBoxOssetian.Location = new System.Drawing.Point(904, 27);
+            this.pictureBoxOssetian.Name = "pictureBoxOssetian";
+            this.pictureBoxOssetian.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxOssetian.TabIndex = 2;
+            this.pictureBoxOssetian.TabStop = false;
+            // 
+            // pictureBoxArmenian
+            // 
+            this.pictureBoxArmenian.Location = new System.Drawing.Point(611, 344);
+            this.pictureBoxArmenian.Name = "pictureBoxArmenian";
+            this.pictureBoxArmenian.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxArmenian.TabIndex = 3;
+            this.pictureBoxArmenian.TabStop = false;
+            // 
+            // pictureBoxGeorgian
+            // 
+            this.pictureBoxGeorgian.Location = new System.Drawing.Point(904, 344);
+            this.pictureBoxGeorgian.Name = "pictureBoxGeorgian";
+            this.pictureBoxGeorgian.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxGeorgian.TabIndex = 4;
+            this.pictureBoxGeorgian.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bebas Neue Bold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(614, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "картинка со словом на русском языке";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bebas Neue Bold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(900, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(269, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "картинка со словом на осетинском языке";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bebas Neue Bold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(607, 612);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(265, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "картинка со словом на армянском языке";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bebas Neue Bold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(900, 612);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(270, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "картинка со словом на грузинском языке";
+            // 
+            // textBoxRussianWord
+            // 
+            this.textBoxRussianWord.Font = new System.Drawing.Font("Bebas Neue Bold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxRussianWord.Location = new System.Drawing.Point(55, 344);
+            this.textBoxRussianWord.Name = "textBoxRussianWord";
+            this.textBoxRussianWord.Size = new System.Drawing.Size(498, 35);
+            this.textBoxRussianWord.TabIndex = 9;
+            // 
+            // textBoxOssetianWord
+            // 
+            this.textBoxOssetianWord.Font = new System.Drawing.Font("Bebas Neue Bold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxOssetianWord.Location = new System.Drawing.Point(55, 456);
+            this.textBoxOssetianWord.Name = "textBoxOssetianWord";
+            this.textBoxOssetianWord.Size = new System.Drawing.Size(498, 35);
+            this.textBoxOssetianWord.TabIndex = 10;
+            // 
+            // textBoxArmenianWord
+            // 
+            this.textBoxArmenianWord.Font = new System.Drawing.Font("Bebas Neue Bold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxArmenianWord.Location = new System.Drawing.Point(55, 568);
+            this.textBoxArmenianWord.Name = "textBoxArmenianWord";
+            this.textBoxArmenianWord.Size = new System.Drawing.Size(498, 35);
+            this.textBoxArmenianWord.TabIndex = 11;
+            // 
+            // textBoxGeorgianWord
+            // 
+            this.textBoxGeorgianWord.Font = new System.Drawing.Font("Bebas Neue Bold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxGeorgianWord.Location = new System.Drawing.Point(55, 680);
+            this.textBoxGeorgianWord.Name = "textBoxGeorgianWord";
+            this.textBoxGeorgianWord.Size = new System.Drawing.Size(498, 35);
+            this.textBoxGeorgianWord.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bebas Neue Bold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(63, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(272, 28);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "введите слово на русском языке";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bebas Neue Bold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(63, 424);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(299, 28);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "введите слово на осетинском языке";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bebas Neue Bold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(63, 538);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(294, 28);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "введите слово на армянском языке";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Bebas Neue Bold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(63, 648);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(300, 28);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "введите слово на грузинском языке";
+            // 
+            // buttonSelection
+            // 
+            this.buttonSelection.Font = new System.Drawing.Font("Bebas Neue Bold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSelection.Location = new System.Drawing.Point(68, 112);
+            this.buttonSelection.Name = "buttonSelection";
+            this.buttonSelection.Size = new System.Drawing.Size(176, 91);
+            this.buttonSelection.TabIndex = 17;
+            this.buttonSelection.Text = "ВЫБРАТЬ ИЗОБРАЖЕНИЕ";
+            this.buttonSelection.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(281, 27);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox.TabIndex = 18;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonSaveResult
+            // 
+            this.buttonSaveResult.Font = new System.Drawing.Font("Bebas Neue Bold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSaveResult.Location = new System.Drawing.Point(904, 649);
+            this.buttonSaveResult.Name = "buttonSaveResult";
+            this.buttonSaveResult.Size = new System.Drawing.Size(250, 66);
+            this.buttonSaveResult.TabIndex = 19;
+            this.buttonSaveResult.Text = "СОХРАНИТЬ РЕЗУЛЬТАТ";
+            this.buttonSaveResult.UseVisualStyleBackColor = true;
             // 
             // FormMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1223, 759);
+            this.Controls.Add(this.buttonSaveResult);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonSelection);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxGeorgianWord);
+            this.Controls.Add(this.textBoxArmenianWord);
+            this.Controls.Add(this.textBoxOssetianWord);
+            this.Controls.Add(this.textBoxRussianWord);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxGeorgian);
+            this.Controls.Add(this.pictureBoxArmenian);
+            this.Controls.Add(this.pictureBoxOssetian);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.pictureBoxRussian);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormMenu";
             this.Text = "IronPainter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRussian)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOssetian)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxArmenian)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGeorgian)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxRussian;
+        private System.Windows.Forms.Button buttonResult;
+        private System.Windows.Forms.PictureBox pictureBoxOssetian;
+        private System.Windows.Forms.PictureBox pictureBoxArmenian;
+        private System.Windows.Forms.PictureBox pictureBoxGeorgian;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxRussianWord;
+        private System.Windows.Forms.TextBox textBoxOssetianWord;
+        private System.Windows.Forms.TextBox textBoxArmenianWord;
+        private System.Windows.Forms.TextBox textBoxGeorgianWord;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonSelection;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonSaveResult;
     }
 }
 
