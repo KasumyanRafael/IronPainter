8a86bfe4fa755a57374b95bf7256e85678cf140a
Ilona Bekoeva <beklona30@gmail.com>
2022-12-16 16:03:56 +0300

    реализовала интерфейс формы FormEditorGameOssetianLoto (РЕДАКТОР ИГРЫ "ОСЕТИНКОЕ ЛОТО")

--- IronPainter/FormEditorGameOssetianLoto.Designer.cs
+++ IronPainter/FormEditorGameOssetianLoto.Designer.cs
@@ -28,12 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxOssetianWord = new System.Windows.Forms.TextBox();
+            this.textBoxRussianWord = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSelection = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBoxOssetian = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRussian = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBoxWordWithoutSignature = new System.Windows.Forms.PictureBox();
+            this.buttonSaveResult = new System.Windows.Forms.Button();
+            this.buttonResult = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOssetian)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRussian)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWordWithoutSignature)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxOssetianWord
+            // 
+            this.textBoxOssetianWord.Font = new System.Drawing.Font("Bebas Neue Book", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxOssetianWord.Location = new System.Drawing.Point(135, 261);
+            this.textBoxOssetianWord.Name = "textBoxOssetianWord";
+            this.textBoxOssetianWord.Size = new System.Drawing.Size(378, 35);
+            this.textBoxOssetianWord.TabIndex = 9;
+            // 
+            // textBoxRussianWord
+            // 
+            this.textBoxRussianWord.Font = new System.Drawing.Font("Bebas Neue Book", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxRussianWord.Location = new System.Drawing.Point(135, 151);
+            this.textBoxRussianWord.Name = "textBoxRussianWord";
+            this.textBoxRussianWord.Size = new System.Drawing.Size(378, 35);
+            this.textBoxRussianWord.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bebas Neue Bold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(129, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(364, 34);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "введите слово на осетинском языке";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bebas Neue Bold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(129, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 34);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "введите слово на русском языке";
+            // 
+            // buttonSelection
+            // 
+            this.buttonSelection.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSelection.Font = new System.Drawing.Font("Bebas Neue Bold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSelection.Location = new System.Drawing.Point(135, 686);
+            this.buttonSelection.Name = "buttonSelection";
+            this.buttonSelection.Size = new System.Drawing.Size(378, 60);
+            this.buttonSelection.TabIndex = 11;
+            this.buttonSelection.Text = "ВЫБЕРИТЕ ИЗОБРАЖЕНИЕ";
+            this.buttonSelection.UseVisualStyleBackColor = true;
+            this.buttonSelection.Click += new System.EventHandler(this.buttonSelection_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Location = new System.Drawing.Point(135, 300);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(378, 378);
+            this.pictureBox.TabIndex = 10;
+            this.pictureBox.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bebas Neue Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(748, 418);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(301, 24);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "картинка со словом на осетинском языке";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bebas Neue Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(176, 418);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(277, 24);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "картинка со словом на русском языке";
+            // 
+            // pictureBoxOssetian
+            // 
+            this.pictureBoxOssetian.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxOssetian.BackColor = System.Drawing.Color.White;
+            this.pictureBoxOssetian.Location = new System.Drawing.Point(749, 115);
+            this.pictureBoxOssetian.Name = "pictureBoxOssetian";
+            this.pictureBoxOssetian.Size = new System.Drawing.Size(300, 300);
+            this.pictureBoxOssetian.TabIndex = 25;
+            this.pictureBoxOssetian.TabStop = false;
+            // 
+            // pictureBoxRussian
+            // 
+            this.pictureBoxRussian.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxRussian.BackColor = System.Drawing.Color.White;
+            this.pictureBoxRussian.Location = new System.Drawing.Point(163, 115);
+            this.pictureBoxRussian.Name = "pictureBoxRussian";
+            this.pictureBoxRussian.Size = new System.Drawing.Size(300, 300);
+            this.pictureBoxRussian.TabIndex = 24;
+            this.pictureBoxRussian.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bebas Neue Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(453, 684);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(280, 24);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "слово на осетинском без изображения";
+            // 
+            // pictureBoxWordWithoutSignature
+            // 
+            this.pictureBoxWordWithoutSignature.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxWordWithoutSignature.BackColor = System.Drawing.Color.White;
+            this.pictureBoxWordWithoutSignature.Location = new System.Drawing.Point(327, 480);
+            this.pictureBoxWordWithoutSignature.Name = "pictureBoxWordWithoutSignature";
+            this.pictureBoxWordWithoutSignature.Size = new System.Drawing.Size(560, 201);
+            this.pictureBoxWordWithoutSignature.TabIndex = 28;
+            this.pictureBoxWordWithoutSignature.TabStop = false;
+            // 
+            // buttonSaveResult
+            // 
+            this.buttonSaveResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSaveResult.Font = new System.Drawing.Font("Bebas Neue Bold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSaveResult.Location = new System.Drawing.Point(763, 652);
+            this.buttonSaveResult.Name = "buttonSaveResult";
+            this.buttonSaveResult.Size = new System.Drawing.Size(450, 104);
+            this.buttonSaveResult.TabIndex = 37;
+            this.buttonSaveResult.Text = "СОХРАНИТЬ РЕЗУЛЬТАТ";
+            this.buttonSaveResult.UseVisualStyleBackColor = true;
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonResult.Font = new System.Drawing.Font("Bebas Neue Bold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonResult.Location = new System.Drawing.Point(-28, 652);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(450, 104);
+            this.buttonResult.TabIndex = 36;
+            this.buttonResult.Text = "ПОСМОТРЕТЬ РЕЗУЛЬТАТ";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            // 
+            // FormEditorGameOssetianLoto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormEditorGameOssetianLoto";
+            this.ClientSize = new System.Drawing.Size(1308, 873);
+            this.Controls.Add(this.buttonSaveResult);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBoxWordWithoutSignature);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBoxOssetian);
+            this.Controls.Add(this.pictureBoxRussian);
+            this.Controls.Add(this.buttonSelection);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.textBoxOssetianWord);
+            this.Controls.Add(this.textBoxRussianWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormEditorGameOssetianLoto";
+            this.Text = "РЕДАКТОР ИГРЫ \"ОСЕТИНКОЕ ЛОТО\"";
+            this.Load += new System.EventHandler(this.FormEditorGameOssetianLoto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOssetian)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRussian)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWordWithoutSignature)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxOssetianWord;
+        private System.Windows.Forms.TextBox textBoxRussianWord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSelection;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBoxOssetian;
+        private System.Windows.Forms.PictureBox pictureBoxRussian;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBoxWordWithoutSignature;
+        private System.Windows.Forms.Button buttonSaveResult;
+        private System.Windows.Forms.Button buttonResult;
     }
 }
